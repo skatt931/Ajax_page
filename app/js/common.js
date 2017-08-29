8df83641a91351e18bf80ecdea7bda62fcce7834
skatt931 <i.kurnytskyi@gmail.com>
2017-08-29 15:04:43 +0300

    Add first functional. Popup with lading data from another html page.

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,24 @@
 $(function() {
 
+    //Declare variables
+    let showHtmlPopup = $('.showHtmlPopup');
+    let showFirstItems = $('.showFirstItems');
+    let showDataInTable = $('.showDataInTable');
+    let showImageSlider= $('.showImageSlider');
+
     vex.defaultOptions.className = 'vex-theme-flat-attack';
 
-    $('.popup').on('click', function(){
-        vex.dialog.alert({
-            message: "OK",
-        });
+    //First popup, showing content from another html page.
+    showHtmlPopup.on('click', function contentFromHtml(){
+       let l = $('.content').load('content.html div', function (data) {
+           vex.dialog.alert({
+               unsafeMessage: data,
+           });
+           console.log(data);
+       });
     });
+
+    //Second popup, showing first 10 items from people.json
+
+
 });
