e9847cb706c807b8ce871d4c6af0b289f9662786
skatt931 <i.kurnytskyi@gmail.com>
2017-09-05 19:28:10 +0300

    almoust over. Fix all things in slideshow, now it working corect. Need to add some better styles to slideshow

--- app/js/common.js
+++ app/js/common.js
@@ -21,7 +21,7 @@ $(function() {
     //3.Second popup, showing first 10 items from people.json
     FirstItemsPopup.on('click', function showFirstItems(){
         $.getJSON('people.json', function (data) {
-            let output = `<ul>`;
+            let output = `<div class="cover"><ul>`;
             $.each(data, function (key, val) {
                 if( key < 10 ){
                     output += `<li>`;
@@ -30,7 +30,7 @@ $(function() {
                     output += `</li>`;
                 }
             })
-            output += '</ul>'
+            output += '</ul></div>'
             vex.dialog.alert({
                 unsafeMessage: output,
             });
@@ -42,14 +42,14 @@ $(function() {
     DataInTablePopup.on('click', function showDataInTable(){
         $.getJSON('people.json', function (data) {
             let item = 0;
-            let output = `<table id="myTable" border="1">`;
-                output += '<thead><tr><th>Name</th><th >Position</th><th>Age</th><th>Note</th></tr></thead><tbody>';
+            let output = `<div class="cover"><table id="myTable" border="1">`;
+                output += '<thead style="cursor: pointer"><tr><th>Name</th><th >Position</th><th>Age</th><th>Note</th></tr></thead><tbody>';
                 $.each(data, function (key, val) {
                     output += `<tr data-item="${item++}">`;
                     output += `<td data-name>${val.name || 'name'}  </td> <td data-position>${val.position || 'position'}  </td> <td data-age>${val.age || 'age'}  </td>  <td data-note>${val.note || '"There is no notes"'}  </td>`;
                     output += '</tr>';
                 });
-            output += `</table>`;
+            output += `</table></div>`;
             vex.dialog.alert({
                 unsafeMessage: output,
             });
@@ -60,5 +60,73 @@ $(function() {
         });
 
     });
+
+    //5.Fourth popup Image Slider Popup
+    ImageSliderPopup.on('click', function showImageSliderPopup() {
+        $.getJSON('slides.json', function (data) {
+            var counter = 0;
+            let output = `<div class="image-sliders">`;
+
+            if(counter <= 0 ){
+                if(counter >= 3) {
+                    counter = 0;
+                } else {
+                    if(data[counter].type == 'html') {
+                        output += `<div id="text-slide">${data[counter].content}</div>`;
+                        output += `<img src="${data[counter].content}" class="img-fluid" id="main-img" alt="Responsive image" style="display: none">`
+                    } else {
+                        output += `<img src="${data[counter].content}" class="img-fluid" id="main-img" alt="Responsive image">`
+                    }
+                    counter++;
+                }
+            }
+            output += `</div>`
+
+            vex.dialog.alert({
+                unsafeMessage: output,
+            });
+
+            //Selectors in modal window
+            let $fluid = $('#main-img');
+            let $textSlide = $('#text-slide');
+            var timerID;
+
+            //Starting slide show
+            function startSlideShow() {
+                timerID = setInterval(function(){
+                    console.log(counter);
+                    if(counter >= 0) {
+                        if(counter > 3) {
+                            counter = 0;
+                            $textSlide.css('display', 'block');
+                            $fluid.css('display', 'none');
+                            counter++;
+                        } else {
+                            $textSlide.css('display', 'none');
+                            $fluid.css('display', 'block');
+                            $fluid.attr('src', data[counter++].content);
+                        }
+                    }
+                },2000);
+            }
+            startSlideShow();
+
+
+            //Functions to start or stop slideshow
+            $('.image-sliders').mouseenter(function(){
+                console.log("enter");
+                clearInterval(timerID);
+            });
+            $('.image-sliders').mouseleave(function(){
+                console.log("leave");
+                startSlideShow();
+            });
+            $('div:not(.vex-content)').on('click', function() {
+                console.log('"not" working');
+                clearInterval(timerID);
+            });
+        });
+
+    })
 
 });
